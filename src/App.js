86699713d21123c68a176ea152c3d6b12ad0a8e9
kath <katherine.linw@gmail.com>
2022-05-17 10:09:41 +1000

    Clean some code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar'
 import {BrowserRouter as Router} from 'react-router-dom'
 import Linkhub from './Linkhub';
-import Home from './pages';
 import HomePage from './HomePage'
 
 function App() {
