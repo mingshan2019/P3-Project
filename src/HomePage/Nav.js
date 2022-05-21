20ed90daa4bf985996c1d28fa0730ca0f3892d4b
kath <katherine.linw@gmail.com>
2022-05-21 12:24:15 +1000

    Add avatar

--- src/HomePage/Nav.js
+++ src/HomePage/Nav.js
@@ -12,11 +12,15 @@ export default function Nav() {
 
   
   const { token, setToken } = useToken();
+  const t=token.charAt(0)
   const {Em, setEm} = useEmail();
+
+  console.log(token);
 
     return (
         <div>
-        <div style={{background: 'black', height: '20%',padding:'2% 20% 2% 8%',display:'flex'}}>
+        <div style={{background: 'black', height: '20%',display:'flex'}}>
+        <div style={{padding:'2% 20% 2% 8%',width:'70%'}}> 
         {/* <h1 style={{fontFamily:'Arial',color:'white'}}>LinkHub</h1> */}
         <Link to='/' style={{fontFamily:'Arial',color:'white'}}>LinkHub</Link>
         <drop/>
@@ -25,11 +29,12 @@ export default function Nav() {
         <Link to='/Blog' style={{color: 'white',display:'inline',paddingLeft:'10%'}}>Blog</Link>
         <Link to='/Price' style={{color: 'white',display:'inline',paddingLeft:'10%'}}>Price</Link>
         <Link to='/Help' style={{color: 'white',display:'inline',paddingLeft:'10%',paddingRight:'16%'}}>Help</Link>
-        <div style={{color:'red',paddingRight:'4%'}}>{token}</div>
-        <button style={{ background:'grey'}} onClick={() => {localStorage.clear(); sessionStorage.clear();
+       </div>
+        <div style={{color:'white',marginRight:'8%',borderRadius:'20px', background:'grey',width:'30px',height:'30px',paddingTop:'0.25%',paddingLeft:'0.5%',marginTop:'1.5%'}}>{t}</div>
+        <button style={{ background:'grey',marginTop:'1.5%',height:'2%'}} onClick={() => {localStorage.clear(); sessionStorage.clear();
           window.location.reload(false);
         }}>LogOff</button>
-        <FaBars style={{color: 'white'}}/>
+        <FaBars style={{color: 'white',float:'right',marginLeft:'8%'}}/>
         </div>
         </div>
     )
