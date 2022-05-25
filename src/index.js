9bb2ddbd4e136dcfd623f70abe001dc61a6e008c
kath <katherine.linw@gmail.com>
2022-05-26 01:31:44 +1000

    Init clean

--- src/index.js
+++ src/index.js
@@ -1,52 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Linkhub from './Linkhub';
-import reportWebVitals from './reportWebVitals';
-//import '../fonts/NunitoSans-Black.ttf'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import SignUp from './SignUp'
-import Login from './Login'
-import Template from './TemplatePage'
-import Blog from './BlogPage'
-import Price from './PricePage'
-import Help from './HelpPage'
-import Design from './DesignPage'
-import Share from './SharePage'
-import ForgetPassWord from './ForgetPassword'
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 
+import Welcome from './WelcomePage'
+import Test from './TestPage'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="SignUp" element={<SignUp />} />
-      <Route path="Login" element={<Login/>} />
-      <Route path="Template" element={<Template />} />
-      <Route path=":templateId" element={<Share />} />
-      <Route path="Blog" element={<Blog />} />
-      <Route path="Price" element={<Price />} />
-      <Route path="Help" element={<Help />} />
-      <Route path="Design" element={<Design />} />
-      <Route path="ForgetPassWord" element={<ForgetPassWord />} />
-
-
-    </Routes>
+  <Routes>
+      <Route path="/" element={<Welcome/>} />
+  </Routes>
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
