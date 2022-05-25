71f4222c910fe0380db1fd6108589cf4f6131be6
kath <katherine.linw@gmail.com>
2022-05-25 14:15:58 +1000

    Encaps Link

---
+++ src/LinkComponent/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class LinkComponent extends Component {
+
+  constructor(props) {
+  super(props);
+  }
+
+  render() {
+    return (
+      <div style={{paddingTop:'20px'}}>
+      <div style={{background:'grey',width:'220px',height:'35px',borderRadius:'5px',padding:'10px'}}>{this.props.link} </div>
+      </div>
+    )
+  }
+}
--- src/TemplatePage/index.js
+++ src/TemplatePage/index.js
@@ -1,15 +1,18 @@
-import React, { useState,useEffect } from 'react';
-import AddTab from '../AddTab';
+import React, { useState,useEffect } from 'react'
+import AddTab from '../AddTab'
 import phone2 from '../phone2.jpg'
 import Nav from '../HomePage/Nav'
 import image from '../images/cool-background.png'
+import LinkComponent from '../LinkComponent'
 const lists = ["1",'2'];
 
 function Template() {
   const[link, setLink] = useState(0);
   const[lists, setLists] = useState(["a","b"]);
   const listItems = lists.map((item) =>
-  <li key="{item}">{item}</li>
+  <li key="{item}">
+    <LinkComponent link={item}/>  
+  </li>
 );
 const [urlinks,seturLink]=useState([]);
 function handleClick(){
@@ -45,16 +48,17 @@ useEffect(() => {
           <AddTab/>     
           {/* <div style = {{marginBottom : '10px', height: '400px',width:'400px',backgroundSize: 'cover',
 backgroundRepeat: 'no-repeat',backgroundImage:"url('https://i.pinimg.com/474x/c6/09/c9/c609c9d68c49585593799c61dd96b0a3.jpg')"}}> */}
-          <ul>
-          {listItems}
-          </ul>
+
           {/* </div> */}
+          <div style={{marginTop:'80%'}}>
+
         <input 
               onChange = {event => setLink(event.target.value)}
         />
         <button
           onClick = {handleClick}
-        >add</button>
+        >add link</button>
+        </div>
      
           {/* <div id="img" style={{marginTop: '20px'}}><img src={phone2}/></div>   */}
          </div>
@@ -63,12 +67,9 @@ backgroundRepeat: 'no-repeat',backgroundImage:"url('https://i.pinimg.com/474x/c6
          <div style={{background:'white',width:'280px',height:'580px',margin:'9px',padding:'30px',borderRadius:'12px',backgroundImage:`url(${image})` }}>
          <div style={{paddingLeft:'80px',paddingTop:'10px',paddingBottom:'120px'}}><div style={{background:'grey',borderRadius:'60px',width:'60px',height:'60px',padding:'12px'}}>Kath</div></div>
          <div style={{background:'grey',width:'220px',height:'35px',borderRadius:'5px',padding:'10px'}}>{urlinks}</div> 
-         <br/>   
-         <div style={{background:'grey',width:'220px',height:'35px',borderRadius:'5px',padding:'10px'}}>Link2</div> 
-         <br/> 
-         <div style={{background:'grey',width:'220px',height:'35px',borderRadius:'5px',padding:'10px'}}>Link3</div>   
-         <br/> 
-         <div style={{background:'grey',width:'220px',height:'35px',borderRadius:'5px',padding:'10px'}}>Link4</div>   
+         <ul>
+          {listItems}
+          </ul>
         </div>  
  
          
