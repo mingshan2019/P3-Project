c8f39a959547d0129a90b7c0ad0a73795bc1541b
kath <katherine.linw@gmail.com>
2022-05-28 21:02:48 +1000

    Add mention

--- src/DesignPage/index.js
+++ src/DesignPage/index.js
@@ -1,55 +1,89 @@
 import React, { useState } from 'react'
-import {Layout,Button,Input} from 'antd'
+import { Layout, Button, Input, Space, Mentions } from 'antd'
 import Nav from '../Nav'
 import PhoneItem from '../PhoneFrame/PhoneItem';
 import PhoneFrame from '../PhoneFrame';
 import Designer from './Designer';
 import { SketchPicker } from 'react-color';
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 
-const {Content,Footer} = Layout
+const { Content, Footer } = Layout
 
-
+const { Option } = Mentions;
+const MOCK_DATA = {
+  '@': ['Panny', 'Zoe', 'James'],
+  '#': ['Facebook', 'Twitter', 'Youtube','Weibo'],
+};
 
 export default function Design(props) {
-  const[link, setLink] = useState(0);
-  const[lists, setLists] = useState(["a","b"]);
+
+  const [prefix, setPrefix] = useState('@');
+
+  const onSearch = (_, newPrefix) => {
+    setPrefix(newPrefix);
+  };
+
+  const [link, setLink] = useState(0);
+  const [lists, setLists] = useState(["a", "b"]);
   const listItems = lists.map((item) =>
-  <li key="{item}">
-    <PhoneItem link={item}/>  
-  </li>
-);
-const [color,setColor]=useState('grey');
-const [img,setImg]=useState(`url("https://jrlinkhub.s3.ap-southeast-2.amazonaws.com/1.png")`);
+    <li key="{item}">
+      <PhoneItem link={item} />
+    </li>
+  );
+  const [color, setColor] = useState('grey');
+  const [img, setImg] = useState(`url("https://jrlinkhub.s3.ap-southeast-2.amazonaws.com/1.png")`);
 
-const location = useLocation();
+  const location = useLocation();
 
-function handleClick(){
-  setLists([...lists, link]);
-  console.log("props "+ location.state.portfolioName)
-}
+  function handleClick() {
+    setLists([...lists, link]);
+    console.log("props " + location.state.portfolioName)
+  }
 
   return (
     <Layout>
-        <Nav/>
-        <Content style={{width:'100%',display:'flex'}}>
-        <div style={{paddingLeft:'10%',paddingTop:'2%'}}>
-        <h2>{location.state.portfolioName}</h2>  
-          <SketchPicker onChange={({ hex }) => {setColor(hex)}}/>
-        <br/>  
-        <br/>  
-
-        <Input 
-              onChange = {event => setLink(event.target.value)}
-        />
-        <Button
-          onClick = {handleClick}
-        >add link</Button>
-        </div>      
-
-        <PhoneFrame  color={color} lists={lists} img={img}/>  
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Linkhub ©2022 Copyright</Footer>
-    </Layout>  )
+      <Nav />
+      <Content style={{ width: '100%', display: 'flex' }}>
+        <div style={{ paddingLeft: '10%', paddingTop: '1.5%' }}>
+          <Input addonBefore='Portfolio Name ' defaultValue={location.state.portfolioName}></Input>
+          <br />
+          <br />
+          <SketchPicker onChange={({ hex }) => { setColor(hex) }} />
+          <br />
+          <br />
+          <div>
+          <Input
+            onChange={event => setLink(event.target.value)}
+          />
+          <Button
+            onClick={handleClick}
+          >add link</Button>
+          </div>
+          <br />
+        </div>
+        <PhoneFrame color={color} lists={lists} img={img} />
+        <div style={{paddingTop:'2%', paddingLeft:'10%'}}>
+        <Mentions
+            autosize
+            style={{
+              width: '200%',
+              height:'20%'
+            }}
+            
+            
+            placeholder="input @ to mention people, # to mention tag"
+            prefix={['@', '#']}
+            onSearch={onSearch}
+          >
+            {(MOCK_DATA[prefix] || []).map((value) => (
+              <Option key={value} value={value}>
+                {value}
+              </Option>
+            ))}
+          </Mentions>
+          </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Linkhub ©2022 Copyright</Footer>
+    </Layout>)
 }
--- src/PhoneFrame/index.js
+++ src/PhoneFrame/index.js
@@ -14,7 +14,7 @@ export default function PhoneFrame(props) {
 );
     
     return (
-      <div style={{marginLeft:'20%',marginTop:'2%'}}>
+      <div style={{marginLeft:'15%',marginTop:'2%'}}>
       <div style={{background:'black',width:'300px',height:'600px',float:'right',borderRadius:'12px'}}>
       <div style={{background:'white',width:'280px',height:'580px',margin:'9px',borderRadius:'12px',backgroundImage:props.img }}>
       <div style={{paddingLeft:'40%',paddingTop:'20%',paddingBottom:'120px'}}>
